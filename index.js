71f9a5ef387b3d683e5c50e6407c3dae4175ba6e
Scott Schroeder <snschroeder89@gmail.com>
2019-08-15 13:43:53 -0500

    tidied up

--- index.js
+++ index.js
@@ -4,21 +4,13 @@ function handleThumbnailClick() {
 
         const imgSrc = $(this).find('img').attr('src');
         const imgAlt = $(this).find('img').attr('alt');
-
-        console.log(imgSrc, imgAlt);
-
         $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
 
+
+        //original code - worked for mouse clicks, did not work with tab/enter
         // $('.hero img').attr('src', $(e.target).attr('src'));
         //$('.hero img').attr('alt', $(e.target).attr('alt'));
     });
 }
 
-/*$('.thumbnail').on('keypress', e => {
-    if (e.which === 13) {
-        $('.hero img').attr('src', $(e.target).attr('src'));
-        $('.hero img').attr('alt', $(e.target).attr('alt'));
-    }
-
-})*/
 $(handleThumbnailClick)
