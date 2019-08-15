11dce205fea403bef91fe933ab8d0beb3d5a1f51
Scott Schroeder <snschroeder89@gmail.com>
2019-08-15 13:41:36 -0500

    got the cats working

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-$('.thumbnail').on('click', e => {
+function handleThumbnailClick() {
 
-    $('.hero img').attr('src', $(e.target).attr('src'));
-    $('.hero img').attr('alt', $(e.target).attr('alt'));
-});
+    $('.thumbnail').on('click', function(event) {
 
-$('.thumbnail').on('keydown', e => {
-    if (e.key === 'Enter') {
+        const imgSrc = $(this).find('img').attr('src');
+        const imgAlt = $(this).find('img').attr('alt');
+
+        console.log(imgSrc, imgAlt);
+
+        $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+
+        // $('.hero img').attr('src', $(e.target).attr('src'));
+        //$('.hero img').attr('alt', $(e.target).attr('alt'));
+    });
+}
+
+/*$('.thumbnail').on('keypress', e => {
+    if (e.which === 13) {
         $('.hero img').attr('src', $(e.target).attr('src'));
         $('.hero img').attr('alt', $(e.target).attr('alt'));
     }
 
-})
+})*/
+$(handleThumbnailClick)
